a7ca259eb1ee153aa47246571b47c5bba2a1a463
gladyswambura <gladyswahito7@gmail.com>
2022-06-19 06:09:23 +0300

    creates the neighborhood model

--- neighborhood/models.py
+++ neighborhood/models.py
@@ -25,6 +25,28 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} -profile'
 
+class NeighbourHood(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=60)
+    admin = models.ForeignKey("Profile", on_delete=models.CASCADE, related_name='hood')
+    hood_logo =  CloudinaryField('profile_photos/', default='profile_photos/user')
+    description = models.TextField()
+    health_info = models.IntegerField(null=True, blank=True)
+    health_officer = models.CharField(max_length=60, null=True, blank=True)
+    police_info = models.IntegerField(null=True, blank=True)
+    police_officer = models.CharField(max_length=60, null=True, blank=True)
 
+    def __str__(self):
+        return f'{self.name} hood'
+
+    def create_neighborhood(self):
+        self.save()
+
+    def delete_neighborhood(self):
+        self.delete()
+
+    @classmethod
+    def find_neighborhood(cls, neighborhood_id):
+        return cls.objects.filter(id=neighborhood_id)
 
 
