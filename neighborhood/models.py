4b346c362afe48b5ef11d27446ac8132db010675
gladyswambura <gladyswahito7@gmail.com>
2022-06-19 06:08:21 +0300

    creates the profile model

--- neighborhood/models.py
+++ neighborhood/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+from pyuploadcare.dj.models import ImageField
+from PIL import Image
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    avatar = models.ImageField(default='default.jpg', upload_to='profile_images')
+    bio = models.TextField()
+
+    def save_profile(self):
+        self.save()
+        
+        img = Image.open(self.photo.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.photo.path)
+
+    def delete_profile(self):
+        self.delete()
+
+
+    def __str__(self):
+        return f'{self.user.username} -profile'
+
+
+
+
