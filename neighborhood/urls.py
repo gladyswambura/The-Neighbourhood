27388c12968923f08a781ff045b0762ffc33118f
gladyswambura <gladyswahito7@gmail.com>
2022-06-19 14:37:53 +0300

    ft: adds the hood-details path

--- neighborhood/urls.py
+++ neighborhood/urls.py
@@ -19,4 +19,20 @@ urlpatterns = [
     path('all-hoods/', views.hoods, name='hood'),
     path('new-hood/', views.create_hood, name='new-hood'),
 
+    # JOIN HOOD && LEAVE HOOD SECTION
+    path('join_hood/<id>', views.join_hood, name='join-hood'),
+    path('leave_hood/<id>', views.leave_hood, name='leave-hood'),
+
+    # HOOD DETIALS SECTION
+    path('hood-details/<hood_id>', views.hood_details, name='hood-details'),
+
+    # CREATE POST SECTION
+    path('<hood_id>/new-post', views.create_post, name='post'),
+
+    # HOOD MEMBERS SECTION
+    path('<hood_id>/members', views.hood_members, name='members'),
+
+    # SEARCH SECTION
+    path('search/', views.search_business, name='search'),
+
 ]
